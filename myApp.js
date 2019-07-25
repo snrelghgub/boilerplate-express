14b1a7de5c66b9be90d05801a0d1632b067d0627
Taizy <48052195+snrelghgub@users.noreply.github.com>
2019-07-25 23:30:07 +1000

    serving HTML file index.html

--- myApp.js
+++ myApp.js
@@ -13,12 +13,20 @@ console.log("Hello World");
 
 
 /** 2) A first working Express Server */
+/*
 app.get("/", function(req, res){
  res.send('Hello Express'); 
 });
-
+*/
 
 /** 3) Serve an HTML file */
+app.get("/", function(req, res){
+        res.sendFile(__dirname + '/views/index.html');
+});
+
+/*
+app.get("/", res.sendFile('/views/index.html'));
+*/
 
 
 /** 4) Serve static assets  */
