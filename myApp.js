d559e700294fd2eeb076d5ed0cf2a600a6a16318
Taizy <48052195+snrelghgub@users.noreply.github.com>
2019-08-14 11:54:12 +1000

    chained middleware to create time server

--- myApp.js
+++ myApp.js
@@ -46,6 +46,12 @@ app.get("/json", function(req,res){
 
 
 /** 8) Chaining middleware. A Time server */
+app.get('/now', function(req, res, next){
+ req.time=new Date().toString();
+ next(); 
+}, function (req,res){
+ res.send({time: req.time}); 
+});
 
 
 /** 9)  Get input from client - Route parameters */
